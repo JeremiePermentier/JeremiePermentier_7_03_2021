3997b69d613f95f63e06de50ee4999b2773b224b
jérémie <permentierj@gmail.com>
2021-03-16 17:55:09 +0100

    Ajout des routes pour les commentaires et les les likes

--- backend/controllers/messageCtrl.js
+++ backend/controllers/messageCtrl.js
@@ -33,76 +33,93 @@ exports.getAllMessages = (req, res, next) => {
 
 exports.getMessage = (req, res, next) => {
     models.Message.findOne({
+        where: { id: req.params.id },
         attributes: ["id", "userId", "message", "imageUrl", "createdAt"],
         include: [
             {
                 model: models.User,
                 attributes: ["pseudo", "id", "avatar"],
             },
-            // {
-            //     model: models.Like,
-            //     attributes: ["userId"],
-            // },
-            // {
-            //     model: models.Comment,
-            //     attributes: ["id", "userId", "comment"],
-            //     order: [["createdAt", "DESC"]],
-            //     include: [
-            //         {
-            //             model: models.User,
-            //             attributes: ["avatar", "pseudo"],
-            //         }
-            //     ]
-            // }
+            {
+                model: models.Like,
+                attributes: ["userId"],
+            },
+            {
+                model: models.Comment,
+                attributes: ["id", "userId", "comment"],
+                order: [["createdAt", "DESC"]],
+                include: [
+                    {
+                        model: models.User,
+                        attributes: ["avatar", "pseudo"],
+                    }
+                ]
+            }
         ]
     })
     .then(message => res.status(200).json(message))
-    .catch(error => res.status(400).json(error))
+    .catch(error => res.status(400).send(error))
 };
 
 exports.createMessage = (req, res, next) => {
     //params
-    let message = req.body.message;
-    let userId = req.body.userId;
-    if(message){
-        console.log(userId)
-        let newMsg = models.Message.create({
-            message: message,
-            UserId: userId
-        })
-    } else {
-        console.log("pas de message")
+    const message = req.body.message;
+    const userId = req.body.userId;
+    const imageUrl = null;
+
+    if(req.file){
+        imageUrl = `${req.protocol}://${req.get('host')}/img/${req.file.filename}`
     }
+
+    models.Message.create({
+        message: message,
+        UserId: userId,
+        imageUrl: imageUrl
+    })
+    .then(() => res.status(201).send({msg: "Votre message à été crée"}))
+    .catch((error) => res.status(400).send(error))
 };
 
 exports.likeMessage = (req, res, next) => {
     const userId = req.body.userId;
     const messageId = req.params.id;
-    const user = models.Like.findOne({
-        where: { userId: userId, messageId: messageId}
-        .then(() => {
-            if (user){
-                models.Like.destroy(
-                    { where: { userId: userId, messageId: messageId}},
-                );
-                res.status(200).send({ msg: "Vous n'aimez plus ce message"})
-            } else {
-                models.Like.create({
-                    userId: userId,
-                    messageId: messageId,
-                });
-                res.status(201).json({ msg: "Vous aimez ce message" });
-            }
-        })
-        .catch((error) => {
-            return res.status(400).send(error);
-        })
-    });
-    
+    models.Like.findOne({
+        where: { userId: req.body.userId, messageId: req.params.id}
+    })
+    .then(user => {
+        if (user){
+            models.Like.destroy(
+                { where: { UserId: req.body.userId, MessageId: req.params.id}},
+            );
+            res.status(200).send({ msg: "Vous n'aimez plus ce message"})
+        } else {
+            models.Like.create({
+                UserId: req.body.userId,
+                MessageId: req.params.id,
+            });
+            res.status(201).json({ msg: "Vous aimez ce message" });
+        }
+    })
+    .catch((error) => {
+        return res.status(400).send(error);
+    })
 };
 
 exports.addComment = (req, res, next) => {
+    const comment = req.body.comment;
+    const pseudo = req.body.pseudo;
+    const userId = req.body.userId;
+    const messageId = req.params.id;
+
     
+    models.Comment.create({
+        comment: comment,
+        pseudo: pseudo,
+        UserId: userId,
+        MessageId: messageId
+    })
+    .then(() => res.status(201).send({msg: "Votre commentaire à été posté"}))
+    .catch((error) => res.status(500).send(error))
 };
 
 exports.updateMessage = (req, res, next) => {
@@ -114,9 +131,29 @@ exports.updateComment = (req, res, next) => {
 };
 
 exports.deleteComment = (req, res, next) => {
-    
+    models.Comment.destroy({
+        where: {id: req.params.id }
+    })
+    .then(() => res.status(200).send({msg: "Votre commentaire à bien été supprimé"}))
+    .catch((error) => res.status(400).send({error}) )
 };
 
 exports.deleteMessage = (req, res, next) => {
     
+    const message = models.Message.findOne({
+        where: {id: req.params.id}
+    })
+    if(message.imageUrl){
+        const filename = post.imageUrl.split("/img")[1];
+        fs.unlink(`img/${filename}`, () => {
+          models.Message.destroy({ where: { id: message.id } });
+          res.status(200).json({ message: "Post supprimé" });
+        });
+    } else {
+        models.Message.destroy({
+            where: {id: req.params.id}
+        })
+        .then(() => res.status(201).send({msg: "Votre message à été supprimé"}))
+        .catch((error) => res.status(400).send(error))
+    }
 };
--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -3,14 +3,14 @@ const messagesCtrl = require("../controllers/messageCtrl");
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 
-router.get("/", auth, messagesCtrl.getAllMessages);
-router.get("/:id", auth, messagesCtrl.getMessage);
+router.get("/", messagesCtrl.getAllMessages);
+router.get("/:id", messagesCtrl.getMessage);
 router.post("/add", auth, multer, messagesCtrl.createMessage);
-router.post("/:id/like", auth, messagesCtrl.likeMessage);
-router.post("/:id/comment", auth, messagesCtrl.addComment);
-router.put("/:id", auth, multer, messagesCtrl.updateMessage);
+router.post("/:id/like", messagesCtrl.likeMessage);
+router.post("/:id/comment", messagesCtrl.addComment);
+router.put("/:id", multer, messagesCtrl.updateMessage);
 router.put("/comment/:id", auth, messagesCtrl.updateComment);
-router.delete("/comment/:id", auth, messagesCtrl.deleteComment);
-router.delete("/:id", auth, multer, messagesCtrl.deleteMessage);
+router.delete("/comment/:id", messagesCtrl.deleteComment);
+router.delete("/:id", multer, messagesCtrl.deleteMessage);
 
 module.exports = router;
